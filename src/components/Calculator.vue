14902800a789d2dc15738231699252a610d004f8
Jose Gabriel Companioni Benitez <gcompanionib@gmail.com>
2021-10-24 17:21:40 -0400

    Created calculator functionalities

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -3,46 +3,132 @@
     <table class="calculator__table" cellspacing="10">
       <tr>
         <td colspan="4">
-          <input class="calculator__table--input" type="text" disabled />
+          <input
+            v-model="actualValue"
+            class="calculator__table--input"
+            type="text"
+            disabled
+          />
         </td>
       </tr>
       <tr>
-        <td class="calculator__button top">C</td>
-        <td class="calculator__button top">+/-</td>
-        <td class="calculator__button top">%</td>
-        <td class="calculator__button right">/</td>
+        <td class="calculator__button top" @click="clear">C</td>
+        <td class="calculator__button top" @click="invert">+/-</td>
+        <td class="calculator__button top" @click="percent">%</td>
+        <td class="calculator__button right" @click="operator">/</td>
       </tr>
       <tr>
-        <td class="calculator__button common">7</td>
-        <td class="calculator__button common">8</td>
-        <td class="calculator__button common">9</td>
-        <td class="calculator__button right">*</td>
+        <td class="calculator__button common" @click="concat">7</td>
+        <td class="calculator__button common" @click="concat">8</td>
+        <td class="calculator__button common" @click="concat">9</td>
+        <td class="calculator__button right" @click="operator">*</td>
       </tr>
       <tr>
-        <td class="calculator__button common">4</td>
-        <td class="calculator__button common">5</td>
-        <td class="calculator__button common">6</td>
-        <td class="calculator__button right">-</td>
+        <td class="calculator__button common" @click="concat">4</td>
+        <td class="calculator__button common" @click="concat">5</td>
+        <td class="calculator__button common" @click="concat">6</td>
+        <td class="calculator__button right" @click="operator">-</td>
       </tr>
       <tr>
-        <td class="calculator__button common">1</td>
-        <td class="calculator__button common">2</td>
-        <td class="calculator__button common">3</td>
-        <td class="calculator__button right">+</td>
+        <td class="calculator__button common" @click="concat">1</td>
+        <td class="calculator__button common" @click="concat">2</td>
+        <td class="calculator__button common" @click="concat">3</td>
+        <td class="calculator__button right" @click="operator">+</td>
       </tr>
       <tr>
-        <td class="calculator__button--colspan-2 common" colspan="2">0</td>
-        <td class="calculator__button common">.</td>
-        <td class="calculator__button right">=</td>
+        <td
+          class="calculator__button--colspan-2 common"
+          colspan="2"
+          @click="concat"
+        >
+          0
+        </td>
+        <td class="calculator__button common" @click="addPoint">.</td>
+        <td class="calculator__button right" @click="equal">=</td>
       </tr>
     </table>
   </div>
 </template>
 
 <script>
+import { reactive, toRefs } from "@vue/reactivity";
 export default {
   setup() {
-    return {};
+    const state = reactive({
+      actualValue: 0,
+      previousValue: 0,
+      operator: undefined,
+      reset: false,
+    });
+    const concat = (e) => {
+      if (state.reset) {
+        state.reset = false;
+        state.actualValue = 0;
+      }
+      const number = e.target.innerText;
+      const temp = state.actualValue.toString() + number.toString();
+      state.actualValue = Number(temp);
+    };
+    const clear = () => {
+      state.actualValue = 0;
+      state.previousValue = 0;
+      state.operator = undefined;
+    };
+    const invert = () => {
+      state.actualValue = state.actualValue * -1;
+    };
+    const percent = () => {
+      state.actualValue = state.actualValue / 100;
+    };
+    const addPoint = () => {
+      if (!state.actualValue.toString().includes(".")) {
+        state.actualValue = state.actualValue.toString() + ".";
+      }
+    };
+    const getResult = () => {
+      console.log(state.actualValue, state.operator, state.previousValue);
+      let value = 0;
+      switch (state.operator) {
+        case "+":
+          value = Number(state.actualValue) + Number(state.previousValue);
+          break;
+        case "-":
+          value = Number(state.previousValue) - Number(state.actualValue);
+          break;
+        case "*":
+          value = Number(state.actualValue) * Number(state.previousValue);
+          break;
+        case "/":
+          value = Number(state.previousValue) / Number(state.actualValue);
+          break;
+      }
+      state.actualValue = value;
+    };
+    const operator = (e) => {
+      if (state.previousValue !== 0) {
+        getResult();
+      }
+      state.previousValue = state.actualValue;
+      state.operator = e.target.innerText;
+      state.reset = true;
+    };
+    const equal = () => {
+      getResult();
+      state.previousValue = 0;
+      state.operator = undefined;
+      state.reset = true;
+    };
+
+    return {
+      concat,
+      ...toRefs(state),
+      clear,
+      invert,
+      percent,
+      addPoint,
+      operator,
+      equal,
+    };
   },
 };
 </script>
